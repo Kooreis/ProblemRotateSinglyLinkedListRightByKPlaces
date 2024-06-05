b8da11ba46d05bb11bb43f42bfb7b583d60c8222
Kooreis <kooreis.official@gmail.com>
2024-06-05 04:09:53 -0500

    This update introduces the LinkedList class and the Add method. The LinkedList class has a head node. The Add method creates a new node with the given data and adds it to the end of the list.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,19 @@
-using System;
-
-public class Node
+public class LinkedList
 {
-    public int Value;
-    public Node Next;
-}
+    Node head;
+
+    public void Add(int data)
+    {
+        Node newNode = new Node() { Value = data };
+        if (head == null)
+            head = newNode;
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
